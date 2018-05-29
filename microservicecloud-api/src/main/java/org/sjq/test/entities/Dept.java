cd46c2cf59a924aecdf27b8154136687e511f7ce
yutian1012 <yutian1012@126.com>
2018-05-29 19:16:47 +0800

    add cloud consumer

--- microservicecloud-api/src/main/java/org/sjq/test/entities/Dept.java
+++ microservicecloud-api/src/main/java/org/sjq/test/entities/Dept.java
@@ -15,5 +15,5 @@ import lombok.experimental.Accessors;
 public class Dept implements Serializable{
 	private Long deptNo;//主键
 	private String dName;//部门名称
-	private String dbSource;//来自哪个数据库，因为微服务架构可以一个服务对应一个数据库，同一个信息被存储到不同的数据库中
+	private String db_source;//来自哪个数据库，因为微服务架构可以一个服务对应一个数据库，同一个信息被存储到不同的数据库中
 }
